2213ccbd46fb7dda6e9cf4a3ba4102067eaf8c85
jaeju.jang <jjj0611@gmail.com>
2020-02-26 19:26:26 +0900

    feat. 사용자에게 수동 구매를 위해 입력 받는 LottoTicketDto를 생성하고, LottoTicketDto로부터 LottoTicket을 생성하는 기능을 추가

--- src/test/java/lotto/domain/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/domain/lotto/LottoFactoryTest.java
@@ -1,16 +1,18 @@
 package lotto.domain.lotto;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.domain.count.Count;
 
-class LottoTicketFactoryTest {
+class LottoFactoryTest {
 
     @Test
     @DisplayName("LottoTicketFactory는 money를 받아 lottoTicket을 발행")
     void lottoTicketFactoryPublishsLottoTickets() {
         Count count = new Count(10, 4);
-        LottoTickets lottoTickets = LottoTicketFactory.publishLottoTickets(count);
+        LottoTickets lottoTickets = LottoFactory.publishLottoTickets(count);
     }
 }
